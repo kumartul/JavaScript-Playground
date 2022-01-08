5ab2d4bf1cee30a36c88ca7e02c46bb3f003abc8
Atul-Kumar-Official <akumarofficial4224@gmail.com>
2022-01-08 18:56:45 +0530

    [Feature]: Favorites List

--- Garage/Applications/GitHub Profiles/js/main.js
+++ Garage/Applications/GitHub Profiles/js/main.js
@@ -6,8 +6,53 @@ const userContainer = document.querySelector('.container .user');
 
 const searchBar = document.getElementById('search');
 
+// Name of the key to store favorites list in the localStorage
+const favoritesListKeyName = "favorites";
+
+// This will be used as a global variable
+// If a user clicks on the favorite button, then the user will be added in the favorites
+// list
+let username;
+
+// Function: Changes the color of the heart icon
+const changeHeartColor = (state, icon) => {
+    if(state === "in"){
+        icon.style.color = "lightgrey";
+    }
+    else{
+        icon.style.color = "orangered";
+    }
+}
+
+// Function: Removes the user from favorite list
+const removeFromFavorites = username => {
+    // Fetch the favorites list as an array
+    let favoritesArr = JSON.parse(localStorage.getItem(favoritesListKeyName));
+
+    // Remove the user from the array
+    favoritesArr.splice(favoritesArr.indexOf(username), 1);
+
+    localStorage.setItem(favoritesListKeyName, JSON.stringify(favoritesArr));
+}
+
+// Function: Adds the user to favorite list
+const addToFavorites = username => {
+    // Fetch the favorites list as an array
+    let favoritesArr = JSON.parse(localStorage.getItem(favoritesListKeyName));
+
+    // If favoritesArr doesn't exists, then initialize an empty array
+    if(!favoritesArr) favoritesArr = [];
+
+    // Add the user to the array
+    favoritesArr.push(username);
+
+    localStorage.setItem(favoritesListKeyName, JSON.stringify(favoritesArr));
+}
+
 // Function: Populates the userContainer
 const populateContainer = data => {
+    // Initialize 'username' global variable to handle further operations
+    username = data.login;
 
     // If 'data' object has a property named 'message', i.e. the user doesn't exists,
     // then, change the message to "User Not Found!"
@@ -26,8 +71,12 @@ const populateContainer = data => {
         company = "None";
     }
 
+    // Remove all margins so that the content doesn't overflows
+    userContainer.style.margin = "0";
+
     userContainer.innerHTML = 
     `
+    <i class = "fa fa-heart" data-state="out"></i>
     <img src = "${data.avatar_url}" alt = "${data.login}'s Profile Pic">
     <p><strong>Username: </strong> ${data.login}</p>
     <p><strong>Name: </strong> ${data.name}</p>
@@ -37,6 +86,35 @@ const populateContainer = data => {
     <p><strong>Company: </strong>${company}</p>
     <p><a href = "${data.html_url}" target = "_blank">View it</a> on GitHub</p>
     `;
+
+    // Grab the reference to the heart icon
+    const heartIcon = userContainer.querySelector('.fa-heart');
+
+    // Add a click event listener to the heart icon
+    /*
+        1. If the user is not in favorites, then add it to favorites and change the color to 
+            orangered
+        2. If the user is in favorites, then remove it from favorites and change the color to
+            lightgrey
+    */
+    heartIcon.addEventListener('click', () => {
+        // Heart's state (in/out)
+        let heartState = heartIcon.getAttribute("data-state");
+
+        changeHeartColor(heartState, heartIcon)
+
+
+        if(heartState === "in"){
+            removeFromFavorites(username);
+
+            heartIcon.setAttribute("data-state", "out");
+        }
+        else{
+            addToFavorites(username);
+            
+            heartIcon.setAttribute("data-state", "in");
+        }
+    });
 }
 
 // Asynchronous Function: Fetches the user details and converts the response to JSON and
@@ -45,7 +123,7 @@ const fetchUser = async user => {
     let response = await fetch(api_url + user);
     let responseData = await response.json();
 
-    populateContainer(responseData);    
+    populateContainer(responseData);
 }
 
 // Add an event listener to the searchBar
